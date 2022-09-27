1e4907db203adb2d7e4644cace4239fc5b794120
wbarrazaj <wbarraza@gmail.com>
2022-09-27 09:29:18 -0300

    .

--- test01.py
+++ test01.py
@@ -14,8 +14,8 @@ db='master'
 puerto=5000
 #drver='Adaptive Server Enterprise'
 #drver='/usr/lib/x86_64-linux-gnu/libodbc.so'
-#drver='/opt/sap/DataAccess64/ODBC/lib/libsybdrvodb.so'
-drver='/opt/sap/OCS-16_0/python/python26_64r/lib/sybpydb.so'
+drver='/opt/sap/DataAccess64/ODBC/lib/libsybdrvodb.so'
+#drver='/opt/sap/OCS-16_0/python/python26_64r/lib/sybpydb.so'
 #drver='Sybase ASE ODBC Driver'
 #drver='FreeTDS'
 
