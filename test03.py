ed902d2947ebb28028686526ff922aca1b4ed159
wbarrazaj <wbarraza@gmail.com>
2022-09-27 18:07:15 -0300

    .

--- test03.py
+++ test03.py
@@ -1,7 +1,13 @@
 from clases.cls_Bdd import BaseDD
+from clases.cls_Bdd2 import BaseDD
 
-database=BaseDD(servidor='Ares2', usuario='root',clave='Emilita01',db='Prueba',puerto='3306',drver='', motor='MariaDB')
+
+#database=BaseDD(servidor='Ares2', usuario='root',clave='Emilita01',db='Prueba',puerto='3306',drver='', motor='MariaDB')
+#print(database)
+#database.ejecutar_query("select 'Hola'")
+
+print("Postgres")
+
+database=BaseDD(servidor='Ares2', usuario='root',clave='Emilita01',db='Prueba',puerto='5432',drver='', motor='Postgres')
 print(database)
 database.ejecutar_query("select 'Hola'")
-
-print("Hola")
